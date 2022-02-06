5d5393d11ff09aa38a039d26ac8ecb22dc2be90d
jon <marek1990kaminski@gmail.com>
2022-02-06 09:22:43 +0100

    extend consumer.py

--- consumer.py
+++ consumer.py
@@ -1,6 +1,10 @@
+import json
+
 import pika
 from pika import URLParameters, BlockingConnection
 from pika.adapters.blocking_connection import BlockingChannel
+
+from main import Product, db
 
 queue_name = 'main'
 
@@ -13,10 +17,34 @@ channel: BlockingChannel = connection.channel()
 
 channel.queue_declare(queue=queue_name)
 
+product_created = 'product_created'
+product_updated = 'product_updated'
+product_deleted = 'product_deleted'
+
 
 def callback(ch, method, properties, body):
     print(f'received in {queue_name}')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == product_created:
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print(product_created)
+
+    elif properties.content_type == product_updated:
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print(product_updated)
+
+    elif properties.content_type == product_deleted:
+        product = Product.query.get(data['id'])
+        db.session.delete(product)
+        db.session.commit()
+        print(product_deleted)
 
 
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
